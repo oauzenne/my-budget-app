9d9c643ccde62a9f7e153358f0b2c418bc15fa99
Olivia Auzenne <oauzenne@gmail.com>
2023-11-08 16:41:15 -0600

    added more functionality, added more buttons

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const form = document.querySelector("[new-item]");
 const expenseForm = document.querySelector("[new-expense-item]");
-
-const table = document.querySelector("[card-table]");
+const table = document.querySelector("[table-body]");
 const expenseTable = document.querySelector("[expense-card-table]");
-
 const cardDisplay = document.querySelector("[card-display-container]");
 const cardTitle = document.querySelector("[card-display-title]");
 const cardList = document.querySelector("[card-display-list]");
- 
 const expenseCard = document.querySelector("[expense-div]");
 const incomeCard = document.querySelector("[income-div]");
 const listTitleIncome = document.querySelector("[list-title-income]");
 const listTitleExpense = document.querySelector("[list-title-expense]");
-
+const submitButtons = document.querySelectorAll("[submit-button]");
+const deleteButton = document.querySelector("[delete-button]");
+const amountInputs = document.getElementsByClassName("dollar-amount");
 const listItems = document.querySelectorAll("#list li");
+const expenseButton = document.querySelector("[add-expense-button]");
+const incomeButton = document.querySelector("[add-income-button]");
+
+const netTotal = [];
+const indexInputList = [];
+
+let dollar = document.querySelector("[dollar]");
 let activeDiv = "Income";
 
 form.addEventListener("submit", (event) => {
@@ -26,12 +32,54 @@ expenseForm.addEventListener("submit", (event) => {
   handleExpenseSubmit();
 });
 
+for (const amountInput of amountInputs) {
+  amountInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+  });
+}
+
+submitButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const clickedButton = event.target.closest("button");
+    submitNumberClick(clickedButton.getAttribute("data-input-index"));
+  });
+});
+
+incomeButton.addEventListener("click", function (event) {
+  const incomeInput = document.querySelector("[add-new]").value;
+  if (!incomeInput) {
+    event.preventDefault();
+    alert("Please enter a value in the input field");
+  }
+});
+
+expenseButton.addEventListener("click", function (event) {
+  const expenseInput = document.querySelector("[add-new-expense]").value;
+  if (!expenseInput) {
+    event.preventDefault();
+    alert("Please enter a value in the input field");
+  }
+});
+
 listItems.forEach((listItem) => {
   listItem.addEventListener("click", () => {
     activeDiv = listItem.innerText;
     renderCards(activeDiv);
   });
 });
+
+function submitNumberClick(buttonIndex) {
+  for (let i = 0; i < amountInputs.length; i++) {
+    const input = amountInputs[i];
+    if (buttonIndex === input.getAttribute("data-input-index")) {
+      netTotal.push(input.value);
+      console.log(netTotal);
+    }
+  }
+}
 
 function handleSubmit() {
   const incomeInput = document.querySelector("[add-new]");
@@ -43,13 +91,42 @@ function handleSubmit() {
   dollarInput.type = "number";
   dollarInput.placeholder = "$$$";
 
+  for (let i = 0; i < amountInputs.length; i++) {
+    const input = amountInputs[i];
+    indexInputList.push(input.getAttribute("data-input-index"));
+  }
+
+  let highestNumber = Math.max(...indexInputList);
+  let newIndex = parseInt(highestNumber) + 1;
+  indexInputList.push(newIndex);
+  dollarInput.setAttribute("data-input-index", newIndex);
+
   dollarInput.classList.add("dollar-amount");
 
   incomeCell.textContent = incomeInput.value;
-  dollarCell.appendChild(dollarInput);
+
+  const submitButton = document.createElement("button");
+  submitButton.textContent = ">";
+  submitButton.classList.add("submit-button-new");
+  submitButton.setAttribute("data-input-index", newIndex);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.classList.add("delete-button");
+  deleteButton.setAttribute("data-input-index", newIndex);
+  console.log(indexInputList);
+  indexInputList.length = 0;
+
+  const newForm = document.createElement("form");
+
+  newForm.appendChild(dollarInput);
+  newForm.appendChild(submitButton);
+  newForm.appendChild(deleteButton);
+  dollarCell.appendChild(newForm);
 
   newRow.appendChild(incomeCell);
   newRow.appendChild(dollarCell);
+
   table.appendChild(newRow);
   incomeInput.value = "";
   return newRow;
@@ -65,13 +142,43 @@ function handleExpenseSubmit() {
   dollarInput.type = "number";
   dollarInput.placeholder = "$$$";
 
+    for (let i = 0; i < amountInputs.length; i++) {
+    const input = amountInputs[i];
+    indexInputList.push(input.getAttribute("data-input-index"));
+    }
+  
+  let highestNumber = Math.max(...indexInputList);
+  let newIndex = parseInt(highestNumber) + 1;
+  indexInputList.push(newIndex);
+  dollarInput.setAttribute("data-input-index", newIndex);
+  
   dollarInput.classList.add("dollar-amount");
 
   expenseCell.textContent = expenseInput.value;
-  dollarCell.appendChild(dollarInput);
+
+
+  const submitButton = document.createElement("button");
+  submitButton.textContent = ">";
+  submitButton.classList.add("submit-button-new");
+  submitButton.setAttribute("data-input-index", newIndex);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.classList.add("delete-button");
+  deleteButton.setAttribute("data-input-index", newIndex);
+  console.log(indexInputList);
+  indexInputList.length = 0;
+
+  const newForm = document.createElement("form");
+
+  newForm.appendChild(dollarInput);
+  newForm.appendChild(submitButton);
+  newForm.appendChild(deleteButton);
+  dollarCell.appendChild(newForm);
 
   newRow.appendChild(expenseCell);
   newRow.appendChild(dollarCell);
+
   expenseTable.appendChild(newRow);
   expenseInput.value = "";
   return newRow;
